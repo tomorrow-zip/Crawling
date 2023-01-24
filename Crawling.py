a6fdebe16429a21f27d14574846cf46bcd2894b0
eunbi lee <led156@naver.com>
2023-01-25 00:45:54 +0900

    Feat: add Crawling

---
+++ Crawling.py
@@ -0,0 +1,61 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.keys import Keys
+from selenium.common.exceptions import NoSuchElementException
+
+import chromedriver_autoinstaller
+
+import requests
+from bs4 import BeautifulSoup as bs
+
+import time
+
+class Driver:
+    def __init__(self, url):
+        self.driver = self.set_chrome_driver_auto()
+        self.driver.get(url)
+        self.driver.implicitly_wait(20)
+
+    def set_url(self, url):
+        self.driver.get(url)
+        self.driver.implicitly_wait(30)
+
+    def set_chrome_driver_auto(self):
+        chromedriver_autoinstaller.install()
+        return webdriver.Chrome()
+
+    def check_exists_by_xpath(self, xpath):
+        try:
+            self.driver.find_element(By.XPATH, xpath)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def check_exists_by_class(self, class_name, element=None):
+        if element==None:
+            element = self.driver
+        try:
+            element.find_element(By.CLASS_NAME, class_name)
+        except NoSuchElementException:
+            return False
+        return True
+
+    def click_by_xpath_until_end(self, xpath):
+        while (self.check_exists_by_xpath(xpath)) :   
+            btn = self.driver.find_element(By.XPATH, xpath)
+            btn.send_keys(Keys.ENTER)
+            time.sleep(0.1)
+
+    def get_by_class_all(self, class_name):
+        elements = self.driver.find_elements(By.CLASS_NAME, class_name)
+        return elements
+
+    def get_by_class(self, class_name):
+        element = self.driver.find_element(By.CLASS_NAME, class_name)
+        return element
+
+    
+
+
--- ikea.py
+++ ikea.py
@@ -1,95 +1,109 @@
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
-from selenium.webdriver.common.keys import Keys
-from selenium.common.exceptions import NoSuchElementException
-
-import chromedriver_autoinstaller
-
-import requests
-from bs4 import BeautifulSoup as bs
-
-import time
 import pandas as pd
+from Crawling import *
+import re, math
 
-class Driver:
-    def __init__(self, url):
-        self.driver = self.set_chrome_driver_auto()
-        self.driver.get(url)
-        self.driver.implicitly_wait(3) # 1초 기다리기
+class Ikea:
+    categori_len = 13
+    categori_idx = [1, 1, 2, 3, 4, 5, 5, 5, 5, 6, 7, 7, 8]
+    categori_url = [
+        'https://www.ikea.com/kr/ko/cat/sofas-armchairs-fu003/',
+        'https://www.ikea.com/kr/ko/cat/armchairs-couches-fu006/',
+        'https://www.ikea.com/kr/ko/cat/sideboards-buffets-console-tables-30454/',
+        'https://www.ikea.com/kr/ko/cat/beds-bm003/',
+        'https://www.ikea.com/kr/ko/cat/wardrobes-19053/',
+        'https://www.ikea.com/kr/ko/cat/cabinets-cupboards-st003/',
+        'https://www.ikea.com/kr/ko/cat/chests-of-drawers-drawer-units-st004/',
+        'https://www.ikea.com/kr/ko/cat/bookcases-shelving-units-st002/',
+        'https://www.ikea.com/kr/ko/cat/trolleys-fu005/',
+        'https://www.ikea.com/kr/ko/cat/tables-desks-fu004/',
+        'https://www.ikea.com/kr/ko/cat/chairs-fu002/',
+        'https://www.ikea.com/kr/ko/cat/bar-furniture-16244/',
+        'https://www.ikea.com/kr/ko/cat/room-dividers-46080/'
+    ]
 
-    def set_chrome_driver_auto(self):
-        chromedriver_autoinstaller.install()
-        return webdriver.Chrome()
+    def __init__(self, idx=[0]):
+        pass
 
-    def check_exists_by_xpath(self, xpath):
-        try:
-            self.driver.find_element(By.XPATH, xpath)
-        except NoSuchElementException:
-            return False
-        return True
+    def get_max_page_url(self, url):
+        list_total_count = bs(requests.get(url).content, 'html.parser').select('.catalog-product-list__total-count')[0].text
+        print(list_total_count, end='. ')
+        list_total_count = re.findall("[0-9]+", list_total_count)
+        print(list_total_count)
+        total_product = int(list_total_count[0])
+        count_product = int(list_total_count[1])
+        if count_product == 24 :
+            page_number = math.ceil((total_product)/24)
+        else :
+            page_number = math.ceil((total_product+36)/48)
+        print(f'product len : {total_product} / ', end='')
 
-    def click_by_xpath_until_end(self, xpath):
-        while (self.check_exists_by_xpath(xpath)) :   
-            btn = self.driver.find_element(By.XPATH, xpath)
-            btn.send_keys(Keys.ENTER)
-            time.sleep(1)
-
-    def get_by_class_all(self, class_name):
-        elements = self.driver.find_elements(By.CLASS_NAME, class_name)
-        return elements
-
-    def get_by_class(self, class_name):
-        element = self.driver.find_element(By.CLASS_NAME, class_name)
-        return element
+        return url+'?page='+str(page_number)
 
 
+    
+    def by_selenium(self, categori=None):
+        driver = Driver('https://www.google.com/')
+        
+        if categori==None:
+            categori = self.categori_url
+
+        for url in categori:
+            print(f'\n--[ {url} ]--')
+            driver.set_url(self.get_max_page_url(url))
+
+            # 상품 리스트 생성
+            product_list = driver.get_by_class_all('plp-fragment-wrapper')
+            print(f'{len(product_list)}. (total / count)')
+
+            
+            product = product_list[0]
+            names = ['pip-image', 'pip-header-section__title--small', 'pip-price__integer', 'pip-product-compact__wrapper-link']
+            for name in names:
+                if (driver.check_exists_by_class(name, product)):
+                    # print(f'{name} is exists')
+                    if name == 'pip-product-compact__wrapper-link':
+                        link = product.find_element(By.CLASS_NAME, 'pip-product-compact__wrapper-link').get_attribute('href')
+                        link_body = bs(requests.get(link).content, 'html.parser')
+                        information = link_body.select('.pip-product-details__paragraph')
+                        measure = link_body.select('.pip-product-dimensions__measurement-wrapper')
+                        if len(information)==0:
+                            print(f'information-length is {len(information)}')
+                        if len(measure)==0:
+                            print(f'measure-length is {len(measure)}')
+                else:
+                    print(f'{name} is not exists')
+
+            
+
+
+def return_product_info_by_selenium(product):
+    # 이미지
+    img = product.find_element(By.CLASS_NAME, 'pip-image').get_attribute('src')
+
+    # 제품명 <span class="pip-header-section__title--small notranslate" translate="no">GRIMSBU 그림스부 </span>
+    name = product.find_element(By.CLASS_NAME, 'pip-header-section__title--small').text
+
+    # 가격 <span class="pip-price__integer">50,000</span>
+    price = product.find_element(By.CLASS_NAME, 'pip-price__integer').text
+
+    # 상품 링크 <a href="https://www.ikea.com/kr/ko/p/grimsbu-bed-frame-grey-20458757/" class="pip-product-compact__wrapper-link">
+    link = product.find_element(By.CLASS_NAME, 'pip-product-compact__wrapper-link').get_attribute('href')
+    
+    # 사이즈 <span class="pip-header-section__description-measurement" data-ga-action="measurements_header_click" data-ga-label="90x200 cm">90x200 cm</span>
+    # sizes.append(product.find_element(By.CLASS_NAME, 'pip-header-section__description-measurement').text)
+
+    # 상품설명 <div class="pip-product-details__container">
+    # information = bs(requests.get(link).content, 'html.parser').select('.pip-product-details__paragraph')
+    # info = ""
+    # for info_ in information:
+    #     info += info_.text
+    # infos.append(info)
+
+    return img, name, price, link
+
+
+    
 
 if __name__ == '__main__':
-    # 드라이버
-    driver = Driver('https://www.ikea.com/kr/ko/cat/single-beds-16285/')
-    
-    # 더보기 눌러서 전체 리스트 보기
-    driver.click_by_xpath_until_end('/html/body/main/div/div/div[4]/div[1]/div/div[3]/a')
-    time.sleep(1)
-
-    # 상품 리스트 뽑아오기
-    product_list = driver.get_by_class_all('plp-fragment-wrapper')
-    # print(f'product len : {len(product_list)}')
-
-    # 각 상품 탐색
-    imgs = []
-    names = []
-    prices = []
-    links = []
-    sizes = []
-    infos = []
-    for product in product_list:
-        # 이미지
-        imgs.append(product.find_element(By.CLASS_NAME, 'pip-image') .get_attribute('src'))
-
-        # 제품명 <span class="pip-header-section__title--small notranslate" translate="no">GRIMSBU 그림스부 </span>
-        names.append(product.find_element(By.CLASS_NAME, 'pip-header-section__title--small').text)
-
-        # 가격 <span class="pip-price__integer">50,000</span>
-        prices.append(product.find_element(By.CLASS_NAME, 'pip-price__integer').text)
-
-        # 상품 링크 <a href="https://www.ikea.com/kr/ko/p/grimsbu-bed-frame-grey-20458757/" class="pip-product-compact__wrapper-link">
-        link = product.find_element(By.CLASS_NAME, 'pip-product-compact__wrapper-link').get_attribute('href')
-        links.append(link)
-        
-        # 사이즈 <span class="pip-header-section__description-measurement" data-ga-action="measurements_header_click" data-ga-label="90x200 cm">90x200 cm</span>
-        sizes.append(product.find_element(By.CLASS_NAME, 'pip-header-section__description-measurement').text)
-
-        # 상품설명 <div class="pip-product-details__container">
-        information = bs(requests.get(link).content, 'html.parser').select('.pip-product-details__paragraph')
-        info = ""
-        for info_ in information:
-            info += info_.text
-        infos.append(info)
-
-    df = pd.DataFrame(list(zip(names, imgs, prices, links, sizes, infos)))
-    
-    df.to_csv('./flist_indexFalse.csv', index=False, header=False)
-
+    ik = Ikea()
+    ik.by_selenium()
