7a6328b5e2dbdcf2a79ac4c69193845629b98f39
eunbi lee <led156@naver.com>
2023-02-04 15:27:10 +0900

    끗

--- main.py
+++ main.py
@@ -31,7 +31,8 @@ category = {'sofas-armchairs-fu003' : 1,
 class IkeaDriver(webdriver.Chrome):
     def set_url(self, url):
         self.get(url)
-        self.implicitly_wait(30)
+        self.implicitly_wait(1000)
+        print(f'get {url}')
 
     def check_exists_by_xpath(self, xpath):
         try:
@@ -80,34 +81,55 @@ def product_list_to_csv(category_url = 'chests-of-drawers-drawer-units-st004'):
     tmp = []
 
     # 더보기 -> 전체 리스트 보기
-    driver.set_url(get_max_page_url('https://www.ikea.com/kr/ko/cat/' + category_url)) # 방법1 : 빠름
+    driver.set_url(get_max_page_url('https://www.ikea.com/kr/ko/cat/' + category_url))
+    #driver.execute_script("window.scrollTo(0, document.body.scrollHeight, 'smooth');")
+    
+    for i in range(100):
+        driver.find_element(By.TAG_NAME, 'body').send_keys(Keys.PAGE_DOWN)
+        time.sleep(0.2)
+
+    html = driver.page_source
+    #soup = bs(requests.get(get_max_page_url('https://www.ikea.com/kr/ko/cat/' + category_url)).content, "html.parser") # 방법1 : 빠름
+    soup = bs(html, 'html.parser')
     # driver.click_by_xpath_until_end("//a[@aria-label='더 보기']") # 방법2
     time.sleep(1)
 
     # 상품 리스트 뽑아오기
-    product_list = driver.find_elements(By.CLASS_NAME, 'plp-fragment-wrapper')
+    product_list = soup.find_all('div', 'plp-fragment-wrapper') #, attrs={'data-testid': 'plp-product-card'}
+    #product_list = soup.select('.plp-fragment-wrapper')
     time.sleep(1)
     print(f'product_len : {len(product_list)}\t', end='')
     product_list_percent = len(product_list)//10
-    i = 0
+    
+    i=0
+    cnt = 0
     for product in product_list:
-        time.sleep(0.1)
         if i%product_list_percent == 0:
             print('|', end='')
 
+        # if product.find_all('div', attrs={'data-testid': 'plp-product-card'}) is None:
+        #     continue
+        #print(f'{i}, {product}', end='\n\n\n\n\n\n\n')
+
         # 제품명 <span class="pip-header-section__title--small notranslate" translate="no">GRIMSBU 그림스부 </span>
-        name = product.find_element(By.CLASS_NAME, 'pip-header-section__title--small').text
+        if product.select_one('.pip-header-section__title--small') is None:
+            #print(f'is noneType')
+            cnt += 1
+            continue
+        #print(f'type : {type(product)}')
+        name = product.select_one('.pip-header-section__title--small').get_text()
         
         # 가격 <span class="pip-price__integer">50,000</span>
-        price = product.find_element(By.CLASS_NAME, 'pip-price__integer').text
+        price = product.select_one('.pip-price__integer').get_text()
 
         # 상품 링크 <a href="https://www.ikea.com/kr/ko/p/grimsbu-bed-frame-grey-20458757/" class="pip-product-compact__wrapper-link">
-        link = product.find_element(By.CLASS_NAME, 'pip-product-compact__wrapper-link').get_attribute('href')
+        link = product.select_one('.pip-product-compact__wrapper-link')['href']
 
         # 상품 링크 바디 가져오기 이미지 / 사이즈 / 상품설명 가져오기
         link_body = bs(requests.get(link).content, 'html.parser')
 
         # 상품 설명 pip-product-summary__description
+        information = ""
         if len(link_body.select('.pip-product-summary__description')) > 0:
             information = link_body.select('.pip-product-summary__description')[0].text
 
@@ -119,11 +141,11 @@ def product_list_to_csv(category_url = 'chests-of-drawers-drawer-units-st004'):
         measure = measure.replace("\n", "")
 
         tmp.append([category_idx, name, price, link, information, measure])
-        i += i
+        i+=1
 
     # to csv
     pd.DataFrame(tmp).to_csv(f'./data/{category_url}.csv', index=False, header=False)
-    print()
+    print(f'\nNoneType : {cnt}')
     # `idx` int AUTO_INCREMENT NOT NULL ,
     # `type_idx` int  NOT NULL ,
     # `name` varchar(60)  NOT NULL ,
