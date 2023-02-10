64ae3b89b22ebfa090ff79f575ff48adb66d68b2
eunbi lee <led156@naver.com>
2023-02-11 00:00:07 +0900

    이미지 크롤러 csv 리드 추가

--- image.py
+++ image.py
@@ -39,7 +39,7 @@ def image_to_file():
     curs = conn.cursor()
     
     # SQL문 실행
-    sql = "SELECT idx, site FROM furniture"
+    sql = "SELECT idx, type_idx, site FROM furniture"
     curs.execute(sql)
     
     # 데이타 Fetch
@@ -48,7 +48,8 @@ def image_to_file():
     # (idx, categori_idx, product, price, link, info, size, status) -> idx, link
     while row:
         furniture_idx = row[0]
-        link = row[1]
+        categori_idx = row[1]
+        link = row[2]
 
         # 링크 이동
         driver.get(link)
@@ -72,6 +73,9 @@ def image_to_file():
 
             # {product_idx}-{cnt}.jpg
             download_image(imgs[cnt], f'./data/image/{furniture_idx}-{cnt}.jpg')
+
+            if cnt == 0:
+                download_image(imgs[cnt], f'./data/model_image/{categori_idx}/{furniture_idx}.jpg')
         
         row = curs.fetchone()
 
@@ -81,7 +85,7 @@ def image_to_file():
     conn.close()
 
 
-def image_to_file_offset(offset):
+def image_to_file_offset(offset, limit='501'):
     opts = ChromeOptions()
     opts.add_argument("--window-size=2560,1440")
     chromedriver_autoinstaller.install()
@@ -95,16 +99,22 @@ def image_to_file_offset(offset):
     curs = conn.cursor()
     
     # SQL문 실행
-    sql = "SELECT idx, site FROM furniture LIMIT 501 OFFSET "+offset # offset : 0, 500, 1000
+    sql = "SELECT idx, type_idx, site FROM furniture LIMIT "+limit+" OFFSET "+offset # offset : 0, 500, 1000
     curs.execute(sql)
     
     # 데이타 Fetch
     row = curs.fetchone()
     
     # (idx, categori_idx, product, price, link, info, size, status) -> idx, link
+    # ConnectionError
     while row:
         furniture_idx = row[0]
-        link = row[1]
+        categori_idx = row[1]
+        link = row[2]
+
+        if link == 'None':
+            row = curs.fetchone()
+            continue
 
         # 링크 이동
         driver.get(link)
@@ -115,6 +125,8 @@ def image_to_file_offset(offset):
         try:
             driver.find_element(By.CLASS_NAME, 'pip-media-grid__button').send_keys(Keys.ENTER)
         except NoSuchElementException:
+            pass
+        except ElementNotInteractableException:
             pass
 
         # 이미지들 뽑아오기
@@ -128,6 +140,10 @@ def image_to_file_offset(offset):
 
             # {product_idx}-{cnt}.jpg
             download_image(imgs[cnt], f'./data/image/{furniture_idx}-{cnt}.jpg')
+
+            if cnt == 0:
+                download_image(imgs[cnt], f'./data/model_image/{categori_idx}/{furniture_idx}.jpg')
+                
         
         row = curs.fetchone()
 
@@ -136,14 +152,80 @@ def image_to_file_offset(offset):
     # Connection 닫기
     conn.close()
 
+# 이케아 이미지
+# ㄴ 카테고리 ID
+# 	ㄴ 0001.jpg
+# 	ㄴ 0002.jpg
+# 	...
+
+
+def image_to_file_offset_by_csv(start=0, end=10539):
+    opts = ChromeOptions()
+    opts.add_argument("--window-size=2560,1440")
+    chromedriver_autoinstaller.install()
+    driver = IkeaDriver(options=opts)
+
+    # CSV 읽어오기
+    tb = pd.read_csv('./furniture.csv', header=None)
+    
+    # (idx, categori_idx, product, price, link, info, size, status) -> idx, link
+    # ConnectionError
+    for i in range(start, end):
+        furniture_idx = tb.iloc[i, 0]
+        categori_idx = tb.iloc[i, 1]
+        link = tb.iloc[i, 4]
+
+        if link == 'None':
+            continue
+
+        # 링크 이동
+        driver.get(link)
+        driver.implicitly_wait(3)
+
+        # 버튼 누르기
+        # <button type="button" class="pip-btn pip-btn--small pip-btn--secondary pip-media-grid__button"><span class="pip-btn__inner"><span class="pip-btn__label">더 보기 </span></span></button>
+        try:
+            driver.find_element(By.CLASS_NAME, 'pip-media-grid__button').send_keys(Keys.ENTER)
+        except NoSuchElementException:
+            pass
+        except ElementNotInteractableException:
+            pass
+
+        # 이미지들 뽑아오기
+        # pip-media-grid__grid 안에 있는 pip-image
+        img = driver.find_element(By.CLASS_NAME, 'pip-media-grid__grid').find_elements(By.CLASS_NAME, 'pip-image')
+        imgs = list(map(lambda x: x.get_attribute('src'), img))
+
+        # 저장 idx, furniture_idx, files, files_cnt, status
+        for cnt in range(len(imgs)):
+            # tmp.append([furniture_idx, img]) 
+
+            # {product_idx}-{cnt}.jpg
+            download_image(imgs[cnt], f'./data/image/{furniture_idx}-{cnt}.jpg')
+
+            if cnt == 0:
+                download_image(imgs[cnt], f'./data/model_image/{categori_idx}/{furniture_idx}.jpg')
+                
+
+    #pd.DataFrame(tmp).to_csv(f'./data/image/{csv_path}.csv', index=False, header=False)
+
 
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1:
-        image_to_file_offset(sys.argv[1]) # offset : 0, 500, 1000
+    if len(sys.argv) == 2:
+        image_to_file_offset_by_csv(int(sys.argv[1])-3)
+    elif len(sys.argv) == 3:
+        image_to_file_offset_by_csv(int(sys.argv[1])-3, int(sys.argv[2])-3)
     else:
-        image_to_file()
+        image_to_file_offset_by_csv()
+    
+    # if len(sys.argv) == 2:
+    #     image_to_file_offset(sys.argv[1]) # offset : 0, 500, 1000
+    # elif len(sys.argv) == 3:
+    #     image_to_file_offset(sys.argv[1], sys.argv[2]) # offset, limit(501)
+    # else:
+    #     image_to_file()
 
     
     
